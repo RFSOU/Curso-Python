4a8b75519e98c07fb040e02c30aadac6384551cf
Rodrigo <35180315+RFSOU@users.noreply.github.com>
2018-01-30 22:35:43 -0300

    Update aula_010_if.py

--- aula_010_if.py
+++ aula_010_if.py
@@ -1,7 +1,7 @@
-#Nessa aula, vamos aprender como utilizar estruturas condicionais simples e compostas nos seus
-#programas em Python.
-#Veja como aplicar os comandos if: e else: no Python.
-
+'''Nessa aula, vamos aprender como utilizar estruturas condicionais simples e compostas nos seus
+programas em Python.
+Veja como aplicar os comandos if: e else: no Python.
+---------------------------------------------------------------------'''
 n1 = float (input('\033[7;35;40m Digite a primeira nota: \033[m'))
 n2 = float (input('\033[7;30;31m Digite a segunda nota: \033[m'))
 m = (n1 + n2)/2
@@ -13,5 +13,6 @@ else:
     print('***'*20)
     print('\033[7;30;41m Sua media foi ruim! ESTUDE MAIS!\033[m ')
 
-# \033[0;30;31m  "cria um padrão com estilo cor de texto e cor de fundo".
-    # \033[m  "encerra na linha um padrão de cores a ser impressa".
+'''\033[0;30;31m  "cria um padrão com estilo cor de texto e cor de fundo".
+     \033[m  "encerra na linha um padrão de cores a ser impressa".'''
+    

6845aa2d8f55485f54e07994018a1014b4e2dcd4
Rodrigo <35180315+RFSOU@users.noreply.github.com>
2018-01-30 22:39:24 -0300

    Update aula_011_cores.py

--- aula_011_cores.py
+++ aula_011_cores.py
@@ -1,12 +1,10 @@
-#Nessa aula, vamos aprender como utilizar os códigos de escape sequence ANSI
-#para configurar cores para os seus programas em Python. Veja como utilizar
-#o código \
-# style 0 none 1 bold 4 anderline
-# text 30 ,31 a 37
-# back 40 a  47
+'''Nessa aula, vamos aprender como utilizar os códigos de escape sequence ANSI
+para configurar cores para os seus programas em Python. Veja como utilizar
+o código \
+style 0 none 1 bold 4 anderline
+text 30,31 a 37
+back 40 a  47
 
-#\033[0:30:41m codigo das cores
+\033[0:30:41m codigo das cores'''
 
 print('\033[0;30;46m Rodrigo\033[m')
-
-

1652f2bea1350a4c770490eb953380b8a07d84f8
Rodrigo <35180315+RFSOU@users.noreply.github.com>
2018-02-02 18:47:56 -0300

    Delete aula_08_randon.py

--- aulas/aula_08_randon.py
+++
@@ -1,6 +0,0 @@
-import random # este modulo gera um numnero randomico que fica
-# mudando conforme é executado, randint (1,10) ele mostra numeros
-# aleatorios de a 10
-num = random.randint(1,10)
-print (num)
-

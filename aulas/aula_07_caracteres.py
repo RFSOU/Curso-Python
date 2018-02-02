92fd61910ef74f1b14f4dac2b03301834030775a
Rodrigo <35180315+RFSOU@users.noreply.github.com>
2018-02-02 18:46:43 -0300

    Delete aula_07_caracteres.py

--- aulas/aula_07_caracteres.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-nome  = input ("Qual é o seu nome?")
-
-print('Prazer em te conhecer {:=^20}!' .format(nome))
-# Obs... o  : ^> < 20 dentro do {} cria um espaço de 20 caracteres
-# onde vai formatar o espaço escrito a variavel nome.

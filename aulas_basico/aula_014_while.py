b688534ada28be0d1ad619ed6207bcfe4518496f
RFSOU <rodrigofsou@gmail.com>
2018-02-03 15:59:59 -0300

    rename

--- aulas_basico/aula_014_while.py
+++ aulas_basico/aula_014_while.py
@@ -1,1 +1,17 @@
+'''
+Aula 14.
+O laço for e o laço while pode ser usado em alguns semalhantes casos a diferença é que o laço for só pode ser usado quando temos o parametro incial eo final, já o laço while e deteminado somente 1 parametro de parada.
+Exemplo:
+'''
+'''
+r = 'S'
+while r == 'S':
+    n = int(input('Digite um valor.'))
+    r = str(input('Quer continuar?[S/N]')).upper()
+print('Fim')
+'''
+n = 1
+while n != 0:
+    n = int(input('Digite um valor:'))
+print('FIM!')
 
--- aulas_basico/aula_06_tipos_primitivos.py
+++ aulas_basico/aula_06_tipos_primitivos.py
@@ -13,3 +13,5 @@ n2 = int (input ('digite uoutro:'))
 s= n1+n2
 #print ('A soma entre ',n1, 'e', n2, 'vale', s)
 print ('a soma entre {} e {} vale {}' .format(n1,n2,s))
+
+

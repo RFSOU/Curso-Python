700b78de3ebeb36f82d99e534d7abb899566afda
Rodrigo <35180315+RFSOU@users.noreply.github.com>
2018-02-03 17:18:54 -0300

    Update exercicio_57_while.py

--- exercicios/exercicio_57_while.py
+++ exercicios/exercicio_57_while.py
@@ -1,8 +1,6 @@
 '''Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'
 caso esteja errado, peça a digitação novamente até ter um valor correto.'''
 sexo = 'a'
-
-
 while sexo != 'M' and sexo != 'F':
     sexo = str(input('Qual éo sexo?[M/F]')).upper()
 if sexo == 'M':

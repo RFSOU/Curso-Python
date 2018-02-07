f10837f47194e9e1861b59dae6ecc2413323e6c7
RFSOU <rodrigofsou@gmail.com>
2018-02-06 23:33:09 -0300

    ujuste de termos

--- exercicios/exercicio_063_fibonacci.py
+++ exercicios/exercicio_063_fibonacci.py
@@ -4,7 +4,10 @@ Faça um programas que leia o numero de termos que você quer e mostre em sequen
 n = float(input('Qual o numero de termos fibonacci você deseja ver '))
 t1 = 0
 t2 = 1
-print('{},{},'.format(t1,t2),end='')
+if n == 1:
+    print(t1)
+else:
+    print('{},{},'.format(t1,t2),end='')
 cont = 3
 while cont <= n:
     cont = cont + 1
